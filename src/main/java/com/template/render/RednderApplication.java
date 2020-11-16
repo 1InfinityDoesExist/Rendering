aede68915e4043d80b6d8bc1bd5ab8a620b0c395
Avinash Patel <avinash.patel@gaiansolutions.com>
2020-11-16 21:09:36 +0530

    baseplate

---
+++ src/main/java/com/template/render/RednderApplication.java
@@ -0,0 +1,13 @@
+package com.template.render;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RednderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RednderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/template/rednder/RednderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.template.rednder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RednderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
