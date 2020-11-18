3ff51d485d0a856c2c56f2a00e24db9c0a6be1a6
Avinash Patel <avinash.patel@gaiansolutions.com>
2020-11-18 22:04:49 +0530

    Controller

---
+++ src/main/java/com/template/render/util/Constants.java
@@ -0,0 +1,11 @@
+package com.template.render.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Constants {
+
+	public final static String ERROR_MESSAGE = "errorMessage";
+	public final static String MESSAGE = "msg";
+	public final static String RESPONSE = "response";
+}
